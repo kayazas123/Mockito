e902a8ace13a566102da5af7da151271c75a3015
ayazk <ayazk@globalkinetic.com>
2018-08-27 16:22:00 +0200

    Initial commit

---
+++ src/main/java/com/junit/ex/junittest/JunittestApplication.java
@@ -0,0 +1,12 @@
+package com.junit.ex.junittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunittestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/junit/ex/junittest/JunittestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.junit.ex.junittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JunittestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
